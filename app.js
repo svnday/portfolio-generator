e5a7e56ab8b153045b7cec2cc8c6cc4900451b36
svnday <40030806+svnday@users.noreply.github.com>
2022-01-06 17:42:37 -0600

    inquirer prompt functionality

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+const inquirer = require('inquirer');
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+/* 
 const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const generatePage = require('./src/page-template');
+const pageHTML = generatePage(name, github);
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw Error(err);
 
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
+*/
+
