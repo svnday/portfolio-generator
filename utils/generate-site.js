40b7605ce2c72dea9216e5554e6dc931a772f511
svnday <40030806+svnday@users.noreply.github.com>
2022-01-09 17:14:40 -0600

    added site generation script to create a full site with styling

---
+++ utils/generate-site.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            // if there's an error reject promise and send error to promise's '.catch' method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure promise doesn't accidentally execute the resolve() functio nas well
+                return;
+            }
+
+            // if everything went well, resolve the promise and send the successful data to the '.then()' method
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'Stylesheet created'
+            });
+        });
+    });
+};
+
+module.exports = { writeFile, copyFile };
