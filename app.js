e19349d5fef79eca3ce9d83d26ab9c3ef88990f1
svnday <40030806+svnday@users.noreply.github.com>
2022-01-09 12:32:42 -0600

    page created from template using data from array generated by user inputs

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 
 const promptUser = () => {
@@ -51,11 +53,11 @@ const promptUser = () => {
 };
 
 const promptProject = portfolioData => {
-console.log(`
-=================
-Add a New Project
-=================
-`);
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
     portfolioData.projects = [];
     // If there's no 'projects' array property, create one
     if (!portfolioData.projects) {
@@ -125,19 +127,12 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
 
-/*
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-const pageHTML = generatePage(name, github);
-
-
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
-*/
 
